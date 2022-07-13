9f151439ac7a1f7712644fa8f51ebf5db8ba09ac
shubham <shubhamjapkarofficial@gmail.com>
2022-07-13 13:42:31 +0530

    new

---
+++ ddd.js
@@ -0,0 +1,11 @@
+let G = [[]];
+let vis = [];
+
+function maximumInvitations(favorite) {
+  let len = favorite.length();
+  var vis = new Array(len).fill(false);
+
+  for (let i = 0; i < len; i++) {
+      G.push();
+  }
+}
